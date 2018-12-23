ee998731e078807587d51c624fddcd2bc98a3830
TSJ <shengjon@163.com>
2018-12-23 17:17:56 +0800

    News list & News detail complate, comment complate

--- README.md
+++ README.md
@@ -2,8 +2,28 @@
 
 + 这是一个基于webpack3.x的 m端 Vue 项目
 
-+ 使用了 git 版本管理工具
++ 使用了 git 管理工具
 
 + 使用的 ui 框架包括 mui mint-ui
 
-+ 入口文件为 main.js 文件， 出口文件为 bundle.js
++ 使用了包括但不限于 Vue-router & Vue-resource & Vue.filter
+
+## 项目内容
+
+### 首页
+
++   mint-ui Swipe 轮播图
+
++   九宫格列表
+
+### 新闻资讯列表
+
++   mui media list  图文列表
+
+### 新闻详情 
+
++   新闻详细内容
+
++   评论列表
+
++   发表评论
--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,32 @@
 <template>
-  <div id="app-container">
+  <div class="app-container">
     <!---顶部 head 区域 -->
     <mt-header fixed title="我的Vue项目真的很棒"></mt-header>
     <!---中间组件区域 -->
 
-
+		<transition>
+			<router-view></router-view>
+		</transition>
     <!--- 底部 Tabbar 区域 -->
 
     <nav class="mui-bar mui-bar-tab">
-			<a class="mui-tab-item mui-active" href="#tabbar">
+			<router-link class="mui-tab-item" to="/home">
 				<span class="mui-icon mui-icon-home"></span>
 				<span class="mui-tab-label">首页</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-chat">
-				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
-				<span class="mui-tab-label">消息</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-contact">
-				<span class="mui-icon mui-icon-contact"></span>
-				<span class="mui-tab-label">通讯录</span>
-			</a>
-			<a class="mui-tab-item" href="#tabbar-with-map">
-				<span class="mui-icon mui-icon-gear"></span>
-				<span class="mui-tab-label">设置</span>
-			</a>
+			</router-link>
+			<router-link class="mui-tab-item" to="/member">
+				<span class="mui-icon mui-icon-contact"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">会员</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/shopcar">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart"></span>
+				<span class="mui-tab-label">购物车</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/search">
+				<span class="mui-icon mui-icon-search"></span>
+				<span class="mui-tab-label">搜索</span>
+			</router-link>
 		</nav>
-21212
   </div>
 
 </template>
@@ -35,8 +36,21 @@
 </script>
 
 <style lang="scss" scoped>
-#app-container {
+.app-container {
   padding: 40px 0 50px 0;
+	overflow-x: hidden;
+}
+.v-enter {
+	opacity: 0;
+	transform: translateX(100%)
+}
+.v-leave-to {
+	opacity: 0;
+	transform: translateX(-100%);
+	position: absolute;
+}
+.v-leave-active,.v-enter-active {
+	transition: all 0.5s ease;
 }
   
 </style>
